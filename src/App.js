2235e292d68325671bbc00fa352c7c9b0a9775c6
johnflentri <jpflentri@gmail.com>
2020-03-27 15:03:15 +0100

    Homepage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Route } from "react-router";
 import './App.css';
 import SignUpForm from './components/SignUpForm'
 import LogInForm from './components/LogInForm';
+import Homepage from './components/Homepage';
 
 class App extends Component {
   render() {
     return <main>
+      <Route path='/' exact component={Homepage} />
       <SignUpForm />
       <LogInForm />
     </main>
