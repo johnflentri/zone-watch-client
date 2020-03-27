2d9ba4bbbb99e0eb916ffc491fbe86ff219c184c
johnflentri <jpflentri@gmail.com>
2020-03-27 14:53:17 +0100

    Existing user can log in

--- src/actions.js
+++ src/actions.js
@@ -1,1 +1,27 @@
-import request from 'superagent'
+import superagent from 'superagent'
+
+export const LOGGED_IN = "LOGGED_IN"
+
+const baseUrl = 'http://localhost:4000'
+
+function loggedIn(jwt) {
+  return {
+    type: LOGGED_IN,
+    payload: jwt
+  }
+}
+
+export function login(username, email, password) {
+  return async function (dispatch) {
+    try {
+      const body = { username, email, password }
+      const response = await superagent
+        .post(`${baseUrl}/login`)
+        .send(body)
+      const action = loggedIn(response.text)
+      dispatch(action)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
