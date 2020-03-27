82eafdf77635373a379852d08c45420df4caa8ec
johnflentri <jpflentri@gmail.com>
2020-03-27 16:19:28 +0100

    Minor adjustment to fix error

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import SignUpForm from './components/SignUpForm'
 import LogInForm from './components/LogInForm';
 import Homepage from './components/Homepage';
+import Locations from './components/Locations'
 
 class App extends Component {
   render() {
     return <main>
       <Route path='/' exact component={Homepage} />
+      <Route path='/locations' component={Locations} />
       <SignUpForm />
       <LogInForm />
     </main>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Locations from './Locations'
 
 export default class Homepage extends Component {
   render() {
     return (
       <div>
         This is the Homepage
-        <Locations />
       </div>
     )
   }
