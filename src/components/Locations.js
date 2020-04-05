c9b26eed07f73d9521b3fdbd4b645b7a2766422f
johnflentri <jpflentri@gmail.com>
2020-04-05 19:51:44 +0200

    Current geolocation set to state

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
-import { getLocations } from "../actions";
+import { getLocations, currentGeo } from "../actions";
 import { Link } from "react-router-dom";
 import CreateLocationContainer from './CreateLocationContainer';
 
 class Locations extends Component {
-  componentDidMount() {
-    this.props.getLocations();
+  state = {
+    lat: '',
+    lng: ''
   }
 
+  componentDidMount() {
+    this.props.getLocations();
+    // this.props.currentGeo();
+    this.getLocation()
+  }
+
+  getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.getCoordinates,
+        this.handleLocationError
+      );
+    } else {
+      alert("Geolocation is not supported");
+    }
+  };
+
+  getCoordinates = position => {
+    const { latitude, longitude } = position.coords;
+    this.setState({ lat: latitude, lng: longitude });
+    // this.props.currentGeo({ lat: latitude, lng: longitude })
+  };
+
+  handleLocationError = error => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert(
+          "Unable to create account. User denied the request for Geolocation."
+        );
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+      default:
+        alert("An unknown error occurred.");
+    }
+  };
+
   render() {
+    console.log("Coordinates in state", this.state)
+
     if (!this.props.locationsList) {
       return <div>Loading...</div>
     }
-
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-
-    function success(pos) {
-      const coord = pos.coords;
-      console.log(`Latitude : ${coord.latitude}`);
-      console.log(`Longitude: ${coord.longitude}`);
-      console.log(`Accurate to approximately ${coord.accuracy} meters.`);
-    }
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
 
     const { locationsList } = this.props
     const mappedLocations = locationsList.map(location => (
@@ -56,6 +84,6 @@ const mapStateToProps = state => ({
   locationsList: state.locations,
 });
 
-const mapDispatchToProps = { getLocations };
+const mapDispatchToProps = { getLocations, currentGeo };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Locations);
