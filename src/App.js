d363285ba685c1296a4c61909311fedf7be7e37a
johnflentri <jpflentri@gmail.com>
2020-05-13 14:40:11 +0200

    Basic toolbar structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
 import { Route } from "react-router";
-import './App.css';
+import './index.css';
 import Homepage from './components/Homepage';
 import Locations from './components/Locations';
 import LocationDetails from './components/LocationDetails'
 import PostDetails from './components/PostDetails'
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import Toolbar from './components/toolbar/Toolbar'
+import SideDrawer from './components/SideDrawer/SideDrawer'
+import Backdrop from './components/Backdrop/Backdrop'
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  }
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen }
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false })
+  }
+
   render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
     return (
-      <div>
-        <Route path='/' exact component={Homepage} />
-        <Route path='/locations' component={Locations} />
-        <Route path='/locationPosts/:id' component={LocationDetails} />
-        <Route path='/posts/:id' component={PostDetails} />
+      <div style={{ height: '100%' }}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        <main style={{ marginTop: '64px' }}>
+          <Route path='/' exact component={Homepage} />
+          <Route path='/locations' component={Locations} />
+          <Route path='/locationPosts/:id' component={LocationDetails} />
+          <Route path='/posts/:id' component={PostDetails} />
+        </main>
       </div>
     )
   }
---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
+import './Toolbar.css'
+
+const toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar__navigation">
+      <div className="toolbar__toggle-button">
+        <DrawerToggleButton click={props.drawerClickHandler} />
+      </div>
+      <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+      <div className="spacer"></div>
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li><a href="/">first</a></li>
+          <li><a href="/">second</a></li>
+          <li><a href="/">third</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
+
+export default toolbar
